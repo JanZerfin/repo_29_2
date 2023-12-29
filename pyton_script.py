2621796b0631a1b481ee716024c1e33f3f7556fe
JanZerfin <Janzerfin9199@web.de>
2023-12-29 07:52:56 +0100

    version3

--- pyton_script.py
+++ pyton_script.py
@@ -1,1 +1,3 @@
 #comment1 in python_script.py
+#comment2 in python_script.py
+#comment3 in python_script.py
--- pyton_script2.py
+++ pyton_script2.py
@@ -1,1 +1,3 @@
 #comment1 in python_script2.py
+#comment2 in python_script2.py
+#comment3 in python_script2.py
